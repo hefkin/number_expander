0992a51ea5d3aa559efd2a4e32ab1f1f6b45ea7a
hefkin <59210240+hefkin@users.noreply.github.com>
2019-12-25 01:11:00 +0400

    Add files via upload

---
+++ Number Expander/Number Expander.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b = 0;
+	double a1;
+	cout << "Введите число, которое требуется развернуть:" << endl;
+	while (!(cin >> a) || (cin.peek() != '\n'))
+	{
+		cin.clear();
+		cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+		cout << "Некорректные данные, попробуйте снова" << endl;
+	}
+	while (a > 0)
+	{
+		a1 = a % 10;
+		a = a / 10;
+		b = b * 10;
+		b = b + a1;
+	}
+	cout << b;
+	return 0;
+}
